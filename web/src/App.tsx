320844d22008b81e024bfa81afa98c5f27e9ad1a
Ednardo Gomes <ednardo@gomes.com>
2022-06-01 17:13:52 -0300

    Definindo a estrutura das rotas da aplicação.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+export function App() {
+  const theme = {
+    primary: "#322153",
+    secondary: "#6C63FF",
+    background: "#F0F0F5",
+    text: "#6C6C80",
+    white: "#FFF"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            Rendenrizando a Home
+          </Route>
+          <Route path="/new">
+            Renderizando a New
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
-export default App;
