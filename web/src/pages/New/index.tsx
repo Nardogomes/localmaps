ab62045fc2050f2c7a54a051dfed01cfa50d95f8
Ednardo Gomes <ednardo@gomes.com>
2022-06-07 17:06:23 -0300

    Implementando a sessão das categorias da page New.

--- web/src/pages/New/index.tsx
+++ web/src/pages/New/index.tsx
@@ -3,6 +3,7 @@ import { LatLngExpression } from "leaflet";
 import { TileLayer, Marker } from 'react-leaflet'
 
 import { Input } from "../../components/Input";
+import { categories } from "./categories";
 
 import {
   Button,
@@ -57,13 +58,34 @@ export function New() {
 
         <Section>Endereço</Section>
 
-        <MapContainer center={[51.505, -0.09] as LatLngExpression} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[-3.7172200, -38.5430600] as LatLngExpression} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={[51.505, -0.09] as LatLngExpression} />
+          <Marker position={[-3.7172200, -38.5430600] as LatLngExpression} />
         </MapContainer>
+
+        <Section>Categoria</Section>
+
+        <CategoryContainer>
+          {categories.map(category => (
+            <CategoryBox
+              key={category.key}
+              onClick={() => {
+                setFormValues(prev => ({...prev, category: category.key}))
+              }}
+              isActive={fomrValues.category === category.key}
+            >
+              <CategoryImage src={category.url} />
+              {category.label}
+            </CategoryBox>
+          ))}
+        </CategoryContainer>
+
+        <ButtonContainer>
+          <Button type="submit">Salvar</Button>
+        </ButtonContainer>
       </Form>
     </Container>
   );
